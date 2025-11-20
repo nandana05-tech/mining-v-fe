2a932bdcd6dce97a77fcbb12467a643ef087ef7f
Firdhabungg <firdhafirmansyah@dicky.gmail.com>
2025-11-20 16:31:56 +0700

    Menambah tampilan Dashboard pada folder scripts/pages/dashboard/ dan juga mengganti logo aplikasi

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -1,9 +1,10 @@
 import HomePage from '../pages/home/home-page';
 import AboutPage from '../pages/about/about-page';
-import LoginPage from '../pages/auth/login/login-page'; 
+import LoginPage from '../pages/auth/login/login-page';
 import RegisterPage from '../pages/auth/register/register-page';
 import ForgotPasswordPage from '../pages/auth/forgotPassword/forgotPassword-page';
 import ResetPasswordPage from '../pages/auth/resetPassword/resetPassword-page';
+import MinePlannerDashboard from '../pages/dashboard/mine-planner/mine-planner-dashboard';
 
 const routes = {
   '/': new HomePage(),
@@ -12,6 +13,7 @@ const routes = {
   '/register': new RegisterPage(),
   '/forgot-password': new ForgotPasswordPage(),
   '/reset-password': new ResetPasswordPage(),
+  '/dashboard-mine': new MinePlannerDashboard(),
 };
 
 export default routes;
